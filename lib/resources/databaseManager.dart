0958435b68c18ba02011677143edeb76fe7ae310
matheusmodanez <matheus.modanez@gmail.com>
2023-07-19 19:47:37 -0300

    sqflite refactors

--- lib/resources/databaseManager.dart
+++ lib/resources/databaseManager.dart
@@ -2,9 +2,20 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseManager {
-  Future<Database> getDatabase() async {
-    final path = join(await getDatabasesPath(), 'winery.db');
-    return openDatabase(path, version: 1, onCreate: _onCreate);
+  DatabaseManager._init();
+
+  static final DatabaseManager instance = DatabaseManager._init();
+  static Database? _database;
+
+  get database async {
+    if (_database != null) return _database;
+
+    return await _initDatabase();
+  }
+
+  _initDatabase() async {
+    return await openDatabase(join(await getDatabasesPath(), 'winery.db'),
+        version: 1, onCreate: _onCreate);
   }
 
   _onCreate(Database db, int version) async {
